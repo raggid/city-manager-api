b098c6b2a6cb629cadb26fd83ba393d1c049bf17
Raggid <antonioraggid@gmail.com>
2021-07-12 13:26:10 -0400

    Initial Commit

---
+++ city-manager/src/main/java/com/artech/citymanager/CityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.artech.citymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityManagerApplication.class, args);
+    }
+
+}
---
+++ city-manager/src/test/java/com/artech/citymanager/CityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artech.citymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
